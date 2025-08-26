7e9a0701884585f64809de1dc078ba0a0dcf1160
Josiah Schatz <josiah.schatz@gmail.com>
2025-08-25 18:42:19 -0700

    updated code

--- src/robot-config.cpp
+++ src/robot-config.cpp
@@ -11,11 +11,11 @@ brain  Brain;
 controller CT1 = controller(primary);
 
 motor L1 = motor(PORT3, ratio6_1, false);
-motor L2 = motor(PORT3, ratio6_1, false);
-motor L3 = motor(PORT3, ratio6_1, false);
-motor R1 = motor(PORT3, ratio6_1, true);
-motor R2 = motor(PORT3, ratio6_1, true);
-motor R3 = motor(PORT3, ratio6_1, true);
+motor L2 = motor(PORT2, ratio6_1, false);
+motor L3 = motor(PORT1, ratio6_1, false);
+motor R1 = motor(PORT6, ratio6_1, true);
+motor R2 = motor(PORT7, ratio6_1, true);
+motor R3 = motor(PORT8, ratio6_1, true);
 // VEXcode generated functions
 
 

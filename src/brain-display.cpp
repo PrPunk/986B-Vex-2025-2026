d29fce0008a1f56ad0bdfcc4072209b0a226dca8
Josiah Schatz <josiah.schatz@gmail.com>
2025-08-25 20:10:46 -0700

    updated code

--- src/brain-display.cpp
+++ src/brain-display.cpp
@@ -1,20 +1,35 @@
 #include "vex.h"
 #include <brain-display.h>
+#include <array>
 
 class BrainDisplay {
     public:
         Page *pages[5];
 
-        int pagesUsed() {
-            int usedPages = 0;
+        std::array<unsigned int, 5> availablePages () {
+            std::array<unsigned int, 5> pagesAvail;
+            unsigned int usedPartsAvail = 0;
             for (int i = 0; i < 5; i++) {
-                usedPages += 1;
+                if (pages[i] != NULL) {
+                    pagesAvail[usedPartsAvail] = i;
+                    usedPartsAvail ++;
+                }
             }
-            return usedPages;
+            return pagesAvail;
         }
 
-        void createPage(int pageId) {
-            
+        void createPage(unsigned int pageId) {
+            std::array<unsigned int, 5> reservedPages = availablePages();
+            if (!(pageId < 5 && pageId >= 0 && pageId != NULL)) {
+                //error: pageId is not within valid range
+                return;
+            }
+            for (int i = 0; i < 5; i++) {
+                if (reservedPages[i] == pageId) {
+                    //error: pageId is in use
+                    return;
+                }
+            }
         }
 
     private:

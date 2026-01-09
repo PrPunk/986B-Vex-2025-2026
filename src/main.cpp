7c19a4ff8526bd0c8d58832d4c4fd01c8a050476
Josiah Schatz <josiah.schatz@gmail.com>
2026-01-08 20:21:57 -0800

    updates

--- src/main.cpp
+++ src/main.cpp
@@ -187,7 +187,7 @@ void autonomous(void) {
     spinIntake();
     moveStraight(33.25, 30);
     stopIntake();
-    turnRobot(100, 30);
+    turnRobot(105, 20);
     outakePistons.set(1);
     moveStraight(-19, 20);
     spinIntakeOutake(1500);
